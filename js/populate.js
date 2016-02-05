faf000126b51a0ebed2f83788131cb64119298e3
badescualexandru <badescu.alexandru@gmail.com>
2016-02-04 23:22:44 -0800

    Add production

---
+++ js/populate.js
@@ -0,0 +1,21 @@
+var div = document.getElementsByClassName('posts')[0],
+p = div.getElementsByTagName('p'),
+data = {
+"items": [
+     {
+       "author": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus"
+     },
+     {
+       "author": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus"
+     },
+     {
+       "author": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus"
+     },
+     {
+       "author": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus. Nulla vitae nisi imperdiet, posuere mi quis, dignissim lectus"
+     }
+  ]
+};
+data.items.forEach(function(v, i) {
+p[i].innerHTML =  v.author;
+});
